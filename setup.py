4c8bcfb53d12cf6b11ce6dc7759670eae9387b93
Eray AYDIN <erayaydin2016@gmail.com>
2017-06-20 03:35:29 +0300

    Initialize setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='XGraphic',
+      version='0.1',
+      description='Switching between nvidia and bumblebee',
+      author='Eray AYDIN',
+      author_email='eray@labkod.com',
+      url='https://www.github.com/erayaydin/xgraphic',
+      data_files=[('stubs', ['stubs/bumblebee.conf', 'stubs/nvidia.conf', 'stubs/.xgraphic-bumblebee', 'stubs/.graphic-nvidia', 'xorg.conf'])]
+     )
